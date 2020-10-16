479e3e4c077e98018dcd262fe3ae4ec297c9266a
Marc Brasseur <maverickmarq@gmail.com>
2020-10-15 21:15:19 -0700

    Add endpoint to use programmatically.

--- app.py
+++ app.py
@@ -46,67 +46,6 @@ sort_filters = {
     'category_desc': 14
 }
 
-def create_tables():
-    """ create tables in the PostgreSQL database"""
-    commands = (
-        """
-        CREATE TABLE users (
-            user_id SERIAL PRIMARY KEY,
-            user_name VARCHAR(255) NOT NULL
-        )
-        """,
-        """ CREATE TABLE parts (
-                part_id SERIAL PRIMARY KEY,
-                part_name VARCHAR(255) NOT NULL
-                )
-        """,
-        """
-        CREATE TABLE part_drawings (
-                part_id INTEGER PRIMARY KEY,
-                file_extension VARCHAR(5) NOT NULL,
-                drawing_data BYTEA NOT NULL,
-                FOREIGN KEY (part_id)
-                REFERENCES parts (part_id)
-                ON UPDATE CASCADE ON DELETE CASCADE
-        )
-        """,
-        """
-        CREATE TABLE vendor_parts (
-                vendor_id INTEGER NOT NULL,
-                part_id INTEGER NOT NULL,
-                PRIMARY KEY (vendor_id , part_id),
-                FOREIGN KEY (vendor_id)
-                    REFERENCES vendors (vendor_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE,
-                FOREIGN KEY (part_id)
-                    REFERENCES parts (part_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE
-        )
-        """)
-    conn = None
-    try:
-        # read the connection parameters
-        params = config()
-        # connect to the PostgreSQL server
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        # create table one by one
-        for command in commands:
-            cur.execute(command)
-        # close communication with the PostgreSQL database server
-        cur.close()
-        # commit the changes
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-
-
-if __name__ == '__main__':
-    create_tables()
-
 
 @APP.route('/', methods=['GET', 'POST'])
 def default_baby_search():
@@ -116,15 +55,21 @@ def default_baby_search():
         return render_template('baby.html'), 200    
     else:
         url = BASE_URL + 'search/' + term
-        print(url)
         lucky = request.form.get('lucky')
         if not lucky:
+            #todo: check for zero
             t = baby_parse_page(url)
             l = 20 if len(t) >= 20 else len(t)
 			
             return render_template('baby-search.html',torrents = t, count = l), 200
         else:
             return lucky_search(baby_parse_page(url))
+            
+@APP.route('/search', methods=['GET'])
+def search_baby_search():
+    query = request.args.get('q')
+    url = BASE_URL + 'search' + q
+    return lucky_search(baby_parse_page(url))
 
 def lucky_search(torrents):
     transmission = requests.get(HOME_BASE)
